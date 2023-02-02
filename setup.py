338af9d3c3ead7fb16d81965dfc47502aac8d368
= <=>
2023-02-02 19:03:50 +0530

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="facts_finder",
-    version="0.0.7",
+    version="0.0.8",
     author="ALIASGAR - ALI",
     author_email="aholo2000@gmail.com",
     description="facts_finder for Networking Geeks",
