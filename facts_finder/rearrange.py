7878b65d50826c8cbd8b76c5061c01f687fbe0c9
= <=>
2023-03-02 14:36:08 +0530

    ver10rearrange update

--- facts_finder/rearrange.py
+++ facts_finder/rearrange.py
@@ -13,9 +13,9 @@ IF_PROPS = {
 	5: ["subnet", "h4block", "v4_helpers", "v6_helpers", ],
 	6: ["ospf_auth", "ospf_auth_type",],
 	7: ["intvrf", "channel_group_interface", "channel_grp", "channel_group_mode"],
-	8: ["description", "int_type", "int_filter", "dist_n", "dist_i",  ],
-	9: ["vlan_index", "vlan_type", "vlan_description",],
-	10:["int_udld",]
+	8: ["description",  "int_filter", ],
+	9:["int_udld",],
+	10: [],
 }
 
 BGP_PROPS = [ 
@@ -24,8 +24,9 @@ BGP_PROPS = [
 ]
 
 VRF_PROPS = [
-	"filter", "vrf", "protocols", "default_rd", "vrf_route_target", "vrf_vpnid", "vrfcolor", "vrf_zonetag", 
-	"vrf_summaries", "vrf_route_xover_blue", "vrf_fw_xover_via_blue_vlan", "vrf_static_default_nexthops", 
+	"filter", "vrf", "protocols", "default_rd", "vrf_route_target", 
+	"vrf_summaries", 
+	"vrf_static_default_nexthops", 
 	"vrf_description", "interfaces",
 ]
 
@@ -58,15 +59,30 @@ def _df_columns_rearrange(pdf_dict, all_cols):
 
 # =====================================================================================================
 
-def rearrange_tables(clean_file):
-	"""rearrange Excel file columns as per interface properties grouping defined globally """
+def rearrange_tables(clean_file, foreign_keys=None):
+	"""rearrange Excel file columns as per interface properties grouping defined globally 
+	provide foreign_keys dictionary with list of columns to be extended.
+	"""
 	pdf_dict = pd.read_excel(clean_file, sheet_name=None)
+	#
 	all_if_cols = _get_all_int_columns()
 	all_bgp_cols = BGP_PROPS
 	all_vrf_cols = VRF_PROPS
+	#
+	# -- add foreign keys
+	if foreign_keys is not None:
+		if "bgp" in foreign_keys:
+			all_bgp_cols.extend(foreign_keys['bgp'])
+		if "vrf" in foreign_keys:
+			all_vrf_cols.extend(foreign_keys['vrf'])
+		if "interfaces" in foreign_keys:
+			all_if_cols.extend(foreign_keys['interfaces'])
+		if "ifs" in foreign_keys:
+			all_if_cols.extend(foreign_keys['ifs'])
+	#
 	all_cols = {
-		'bgp': BGP_PROPS, 
-		'vrf': VRF_PROPS,
+		'bgp': all_bgp_cols, 
+		'vrf': all_vrf_cols,
 		'interfaces': all_if_cols,
 	}
 	pdf_dict = _df_columns_rearrange(pdf_dict, all_cols)
