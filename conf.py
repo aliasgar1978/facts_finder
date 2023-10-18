838af778d1f002f3e67e3b024bc5a9aeede73acf
aliasgar1978 <aholo2000@gmail.com>
2023-10-18 08:02:02 +0530

    .

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2023, AHOLO [ALI]'
 author = 'ALIASGAR'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- facts_finder/__init__.py
+++ facts_finder/__init__.py
@@ -19,4 +19,4 @@ __all__ = [
 	'get_necessary_cmds', 'get_absolute_command',
 	]
 
-__ver__ = "0.1.0"
+__ver__ = "0.1.1"
